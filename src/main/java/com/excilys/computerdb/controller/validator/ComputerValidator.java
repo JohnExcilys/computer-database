432da2257507e372c68f902baccb56a88dbce816
Jean Herrier <jherrier@excilys.com>
2014-03-14 10:04:51 +0100

    Correctifs divers

--- src/main/java/com/excilys/computerdb/controller/validator/ComputerValidator.java
+++ src/main/java/com/excilys/computerdb/controller/validator/ComputerValidator.java
@@ -28,18 +28,20 @@ public class ComputerValidator implements Validator {
 			e.rejectValue("companyId", "label.invCompany", "Invalid company");
 		}
 
+		// Tests à améliorer -- Plante l'appli dans certains cas (date en l'an
+		// 1000)
 		if (obj.getIntroduced() != null) {
 			if (obj.getIntroduced().getYear() < 1000
-					|| obj.getIntroduced().getMonthOfYear() < 0
-					|| obj.getIntroduced().getDayOfMonth() < 0) {
+					|| obj.getIntroduced().getMonthOfYear() < 1
+					|| obj.getIntroduced().getDayOfMonth() < 1) {
 				e.rejectValue("introduced", "label.badDate", "Invalid Date");
 			}
 		}
 
 		if (obj.getDiscontinued() != null) {
 			if (obj.getDiscontinued().getYear() < 1000
-					|| obj.getDiscontinued().getMonthOfYear() < 0
-					|| obj.getDiscontinued().getDayOfMonth() < 0) {
+					|| obj.getDiscontinued().getMonthOfYear() < 1
+					|| obj.getDiscontinued().getDayOfMonth() < 1) {
 				e.rejectValue("discontinued", "label.badDate", "Invalid Date");
 			}
 		}
